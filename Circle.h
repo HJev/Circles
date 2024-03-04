eefb20075c2b5b62bc15ed7c7d687e953774caed
HJev <157658614+HJev@users.noreply.github.com>
2024-03-04 13:55:50 -0500

    Update Circle.h

--- Circle.h
+++ Circle.h
@@ -3,22 +3,35 @@ class Circle
 {
 public: 
 	struct vec2 {
-		float vec2[2];
+		float v[2];
+		vec2 operator- (vec2 a) {
+			return { v[0] - a.v[0], v[1] - a.v[1]};
+		}
+		float operator[] (int a) {
+			return v[a];
+		}
 	};
 	struct vertexs {
-		float v[9];
+		float v[6];
 	};
 	
-	//methods
-	Circle(float, vec2);
-	float getRadius();
+	/* Constructor */
+	Circle(vec2 radius, vec2 centerPos);
+	vec2 getRadius();
 	vec2 getCenter();
 	vertexs getVertx();
-	void setRadius(float);
+	void setRadius(vec2);
 	void setCenter(vec2);
+	void setVelocity(vec2);
+	vec2 getVelocity();
+	void move(float time);
+	bool checkWallColision(float time);
+	bool checkCircleColision(Circle circle, float time);
 
 private:
+	vec2 rotate(vec2 A, float angle);
+	float q_rsqrt(float number);
 	vec2 center;
-	float radius;
+	vec2 radius; // index 0 represents outer radius \n index 1 represents inner radius
+	vec2 velocity;
 };
-
